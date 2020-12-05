ac1c5b6c911a1c8f45468d365903cd768b1ff5a1
Marco Silva <cyberrumor@Marcos-MacBook-Pro.local>
2020-12-04 18:19:43 -0700

    fake price detection

--- bot.py
+++ bot.py
@@ -16,18 +16,29 @@ def notify(url):
 if __name__ == '__main__':
 	logging.basicConfig(format="%(asctime)s %(levelname)s:%(module)s: %(message)s", level=logging.WARNING)
 	s = requests.Session()
-	header = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.16; rv:83.0) Gecko/20100101 Firefox/83.0'}
+	header = [
+		{'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36'}
+	]
 	wait = 6
+	just_notified = False
 	while True:
+		if just_notified:
+			just_notified = False
+			logging.warning('waiting 30 seconds after sending notification...')
+			time.sleep(30)
 		for product in hardware:
 			try:
 				logging.warning('checking {} at {}'.format(product['model'], product['site']))
-				response = s.get(product['url'], headers = header)
+				response = s.get(product['url'], headers = random.choice(header))
 				if product['keyword'] not in response.text:
-					notify(product['url'])
-					logging.warning('IN STOCK: ' + product['model'] + ' at ' + product['site'])
-					logging.warning(product['url'])
-					logging.warning('notification sent')
+					if product['price'] in response.text:
+						notify(product['url'])
+						logging.warning('IN STOCK: ' + product['model'] + ' at ' + product['site'])
+						logging.warning(product['url'])
+						logging.warning('notification sent')
+						just_notified = True
+					else:
+						logging.warning('we are being fed a fake price from ' + product['site'])
 			except Exception as e:
 				logging.warning(e)
 				wait += 1
