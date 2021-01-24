8d37d4d534a964f8bb672aec3c6c373530a2a868
Ubuntu <ubuntu@ip-172-31-7-255.us-east-2.compute.internal>
2021-01-24 20:27:31 +0000

    integrated bs4

--- bot.py
+++ bot.py
@@ -1,41 +1,65 @@
 #!/usr/bin/env python3
 import requests
 import time
-from multiprocessing import Process
 from products import *
 from credentials import *
+from bs4 import BeautifulSoup
 
 def notify(product_name, url):
-	header = {'Content-type': 'application/x-www-form-urlencoded'}
-	payload = {'token': api_token, 'user': user_key, 'message': product_name, 'url': url}
-	response = requests.post('https://api.pushover.net/1/messages.json', headers = header, data = payload)
-	return
+    header = {'Content-type': 'application/x-www-form-urlencoded'}
+    payload = {'token': api_token, 'user': user_key, 'message': product_name, 'url': url}
+    response = requests.post('https://api.pushover.net/1/messages.json', headers = header, data = payload)
+    return
 
-def check_stock(product, header, thread):
-	wait = 11
-	while 1:
-		print(f'THREAD {thread}: {product["site"]} \t {product["model"]} \t CHECKING')
-		try:
-			response = requests.get(product['url'], headers = header)
-			if product['keyword'] not in response.text:
-				if product['price'] in response.text:
-					notify(product['model'], product['url'])
-					print(f'THREAD {thread}: {product["site"]} \t {product["model"]} \t IN STOCK')
-					just_notified = True
-				else:
-					print(f'THREAD {thread}: {product["site"]} \t {product["model"]} \t FAKE PRICE')
-		except Exception as e:
-			print(e)
-			wait += 1
-			print(f'THREAD {thread}: {product["site"]} \t {product["model"]} \t RATE LIMITED WAITING 10')
-			time.sleep(10)
-		print(f'THREAD {thread}: {product["site"]} \t {product["model"]} \t WAITING {wait}')
-		time.sleep(wait)
+
+# returns true or false. Did we get the name we expected?
+def get_seller_name(soup, seller_name, selector, seller_id):
+    seller = soup.find('a', attrs = {selector: seller_id}).string
+    if seller == seller_name:
+        return True
+    else:
+        return False
+
+# returns price if price. Returns false if any error at all.
+def get_price(soup, product):
+    price = soup.find(attrs = product['price_selector'])
+    if product['price_nested']:
+        result = price[product['price_key']]
+    else:
+        result = price.string
+    return result
+
+# returns true if in stock, false otherwise.
+def get_stock(soup, product):
+    inventory = str(soup.find(attrs = {product['oos_selector']: product['oos_id']}))
+    # print(inventory)
+    if product['oos_keyword'] in inventory:
+        return False
+    else:
+        return True
+
+def check_stock(product, header):
+    print(f'{product["site"]} \t {product["model"]} \t CHECKING')
+    response = requests.get(product['url'], headers = header)
+    soup = BeautifulSoup(response.text, 'lxml')
+
+    if 'seller' in list(product.keys()):
+        safe = get_seller_name(soup, product['seller'], product['seller_selector'], product['seller_id'])
+    else:
+        safe = True
+
+    if safe:
+        # check if in stock
+        in_stock = get_stock(soup, product)
+        if in_stock:
+            # notify
+            price = get_price(soup, product)
+            print(f'{product["model"]} {product["url"]} {price}')
 
 if __name__ == '__main__':
-	header = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36'}
-	thread = 1
-	for product in hardware:
-		p = Process(target = check_stock, args = (product, header, thread))
-		p.start()
-		thread += 1
+    header = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36'}
+    wait = 11
+    while True:
+        for product in hardware:
+            check_stock(product, header)
+        time.sleep(wait)
--- products.py
+++ products.py
@@ -1,30 +1,28 @@
 hardware = [
 	{
-	'site': 'newegg',
-	'url': 'https://www.newegg.com/amd-ryzen-7-5800x/p/N82E16819113665',
-	'keyword': 'OutOfStock',
-	'model': '5800x',
-	'price': '<strong>449</strong>'
+	'site': 'bestbuy',
+        'url': 'https://www.bestbuy.com/site/sony-playstation-5-digital-edition-console/6430161.p?skuId=6430161',
+        'oos_keyword': 'Sold Out</button>',
+        'oos_id': 'add-to-cart-button',
+        'oos_selector': 'class',
+        'price_selector': {'class': 'priceView-hero-price priceView-customer-price'},
+        'price_nested': False,
+	'model': 'PS5'
 	},
-	#{
-	#'site': 'bestbuy',
-	#'url': 'https://www.bestbuy.com/site/amd-ryzen-7-5800x-4th-gen-8-core-16-threads-unlocked-desktop-processor-without-cooler/6439000.p?skuId=6439000',
-	#'keyword': 'Sold Out',
-	#'model': '5800x',
-	#'price': '<span aria-hidden="true">$449.00</span>'
-	#},
 	{
-	'site': 'antonline',
-	'url': 'https://www.antonline.com/AMD/Computers/Electronic_Components/Microprocessors/1411790',
-	'keyword': 'Sold Out',
-	'price': '449.99',
-	'model': '5800x'
-	}
-	#{
-	#'site': 'walmart',
-	#'url': 'https://www.walmart.com/ip/AMD-Ryzen-7-5800X-8-core-16-thread-Desktop-Processor/950625545',
-	#'price': '449',
-	#'keyword': 'Out of stock',
-	#'model': '5800x'
-	#}
+	'site': 'walmart',
+        'url': 'https://www.walmart.com/ip/Sony-PlayStation-5-Digital-Edition/493824815',
+	'oos_keyword': '<b>out of stock</b>',
+        'oos_id': 'prod-blitz-copy-message',
+        'oos_selector': 'class',
+	'model': 'PS5',
+        'seller': 'Walmart',
+        #'seller_id': 'ProductSellerInfo-SellerName',
+        #'selector': 'id',
+        'seller_id': 'seller-name',
+        'seller_selector': 'class',
+        'price_selector': {'itemprop': 'price'},
+        'price_nested': True,
+        'price_key': 'content',
+        }
 ]
