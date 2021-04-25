31c86ae830a63047887ee5bcd4249bb9bf0245e7
cyberrumor <cyberrumor@icloud.com>
2021-04-25 16:05:47 -0600

    bypass walmart's bot protection

--- walmart/walmart.py
+++ walmart/walmart.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import requests
 from bs4 import BeautifulSoup
+import time
 
 file = __file__.split('/')[-1]
 print(f'{file} successfully imported')
@@ -21,17 +22,32 @@ def search(product):
 		'facet': 'retailer:Walmart.com',
 		'query': product,
 	}
+
+
+
+    #Cookie: com.wm.reflector="reflectorid:0@lastupd:0@firstcreate:0"; next-day=0; location-data=0
+
+
+
 	header = {
-		'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36',
-		'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-		'accept-encoding': 'gzip, deflate, br',
-		'accept-language': 'en-US,en;q=0.5',
-		'cache-control': 'max-age=0',
-		'connection': 'keep-alive',
-		}
+        'Host': 'www.walmart.com',
+		'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:88.0) Gecko/20100101 Firefox/88.0',
+		'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+		'Accept-Language': 'en-US,en;q=0.5',
+		'Accept-Encoding': 'gzip, deflate, br',
+		'Referer': 'https://www.walmart.com/',
+		'DNT': '1',
+		'Connection': 'keep-alive',
+        'Cookie': 'com.wm.reflector="reflectorid:0@lastupd:0@firstcreate:0"; next-day=0; location-data=0',
+        'TE': 'Trailers',
+        'Pragma': 'no-cache',
+        'Cache-Control': 'no-cache',
+        'Upgrade-Insecure-Requests': '1',
+	}
 
 	s = requests.Session()
 	r = s.get(url)
+	time.sleep(4)
 	r = s.get(url + '/search/', headers = header, params = params)
 	r.raise_for_status()
 
